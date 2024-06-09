7d7770001d5651b4727f8ec2e0f42458042d8a0b
Its-Kirito <edaddojnr@gmail.com>
2024-06-09 13:52:27 +0000

    Initial Commit

---
+++ CSM 158 QUESTION TRIALS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSM 158 QUESTION TRIALS
+//
+//  Created by Edward Addo Jnr on 09/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
